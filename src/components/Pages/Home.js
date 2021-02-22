e3efb9f0e7017c0888dbecce88f479d0f8ea135f
ermias kidane <hadeklte21@gmail.com>
2021-02-22 16:18:54 +0000

    initial commit

---
+++ src/components/Pages/Home.js
@@ -0,0 +1,14 @@
+import HeroSection from "../HeroSection/HeroSection"
+import "../../App.css"
+import Cards from "../Cards/Cards"
+
+const Home = () => {
+    return (
+        <>
+         <HeroSection/>
+         <Cards />
+        </>
+    )
+}
+
+export default Home
