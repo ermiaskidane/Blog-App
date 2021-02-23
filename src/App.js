9455dac3d219ce9b5f69b828d6251d2f7533bd75
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2021-02-23 15:58:50 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react"
 import './App.css';
 import Navbar from "./components/Navbar/Navbar"
 import Home from "./components/Pages/Home"
+import Services from "./components/Pages/Services"
+import Products from "./components/Pages/Products"
+import SignUp from "./components/Pages/SignUp"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 const App = () => {
@@ -11,6 +14,9 @@ const App = () => {
         <Navbar>
           <Switch>
             <Route path="/" exact  component={Home}/>
+            <Route path="/services" exact  component={Services}/>
+            <Route path="/products" exact  component={Products}/>
+            <Route path="/sign-up" exact  component={SignUp}/>
           </Switch>
         </Navbar>
       </Router>
