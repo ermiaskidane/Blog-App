0037c065ee9b195bed8412edfc9eaeb057a095e5
ermias kidane <hadeklte21@gmail.com>
2021-02-23 16:01:51 +0000

    added the services products and signup routes

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,12 +1,14 @@
 import HeroSection from "../HeroSection/HeroSection"
 import "../../App.css"
 import Cards from "../Cards/Cards"
+import Footer from "../Footer/Footer"
 
 const Home = () => {
     return (
         <>
          <HeroSection/>
          <Cards />
+         <Footer />
         </>
     )
 }
