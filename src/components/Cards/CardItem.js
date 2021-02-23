966636f711a27b61b51443e696be378c7ffc013d
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2021-02-23 15:52:53 +0000

    Update CardItem.js

--- src/components/Cards/CardItem.js
+++ src/components/Cards/CardItem.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from "react-router-dom"
 
-function CardItem(props) {
-  return (
-    <>
-      <li className='cards__item'>
-        <Link className='cards__item__link' to={props.path}>
-          <figure className='cards__item__pic-wrap' data-category={props.label}>
-            <img
-              className='cards__item__img'
-              alt='Travel Image'
-              src={props.src}
-            />
-          </figure>
-          <div className='cards__item__info'>
-            <h5 className='cards__item__text'>{props.text}</h5>
-          </div>
-        </Link>
-      </li>
-    </>
-  );
+const CardItem = ({path, label, src, text}) => {
+    return (
+        <> 
+          <li className="cards__item">
+              <Link className="cards__item__link" to={path}>
+                  <figure className="cards__item__pic-wrap" data-category={label}>
+                      <img src={src} alt="Travel Image" className="cards__item__img"/>
+                  </figure>
+                  <div className="cards__item__info">
+                      <h5 className="cards__item__text">{text}</h5>
+                  </div>
+              </Link>
+          </li>  
+        </>
+    )
 }
 
-export default CardItem;
+export default CardItem
