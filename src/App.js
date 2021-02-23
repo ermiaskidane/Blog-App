2bde621546881ad1e2d9113c25c0606072a92c26
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2021-02-23 16:19:46 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,14 +11,13 @@ const App = () => {
   return (
     <>
       <Router>
-        <Navbar>
+        <Navbar />
           <Switch>
             <Route path="/" exact  component={Home}/>
             <Route path="/services" exact  component={Services}/>
             <Route path="/products" exact  component={Products}/>
             <Route path="/sign-up" exact  component={SignUp}/>
           </Switch>
-        </Navbar>
       </Router>
     </>
   );
