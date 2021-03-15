2e7e7d7b7c0a82afb735f1ad1409466c22dafaea
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2021-03-15 11:50:03 +0000

    Update Services.js

--- src/components/Pages/Services.js
+++ src/components/Pages/Services.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Sprite from "../../assets/images/sprite.svg";
 import "../../App.css"
+import "./Services.css"
 
 const Services = () => {
     return (
@@ -9,9 +10,11 @@ const Services = () => {
                 <svg>
                   <use xlinkHref={`${Sprite}#icon-hand`} />
                 </svg>
+                <span>72k</span>
                 <svg>
                   <use xlinkHref={`${Sprite}#icon-bubble`} />
                 </svg>
+                <span>74 </span>
                 <svg>
                   <use xlinkHref={`${Sprite}#icon-bookmark`} />
                 </svg>
@@ -24,9 +27,30 @@ const Services = () => {
                     </div>
                     <div className="detail--body">
                         <figure>
-                            <img src="/" alt="passive income"/>
+                            <img src="/images/img-3.jpg" alt="passive income"/>
                         </figure>
                         <div className="detail--content">
+                            <p>
+                            The highest level of financial freedom is having enough passive income to cover
+                             your living expenses. If you manage to reach that point, you can spend your 
+                             days doing whatever makes you happiest.
+                            If passive income is the key to financial freedom, 
+                            then anyone who can enable others to generate passive 
+                            income will be able to wield massive influence and make themselves 
+                            wealthy. That is why the internet is full of spammy articles and 
+                            videos, promising easy solutions to generate passive income.
+                            </p>
+
+                            <p>
+                            The highest level of financial freedom is having enough passive income to cover
+                             your living expenses. If you manage to reach that point, you can spend your 
+                             days doing whatever makes you happiest.
+                            If passive income is the key to financial freedom, 
+                            then anyone who can enable others to generate passive 
+                            income will be able to wield massive influence and make themselves 
+                            wealthy. That is why the internet is full of spammy articles and 
+                            videos, promising easy solutions to generate passive income.
+                            </p>
                             <p>
                             The highest level of financial freedom is having enough passive income to cover
                              your living expenses. If you manage to reach that point, you can spend your 
