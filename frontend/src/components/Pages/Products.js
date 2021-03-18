1caeb2ee62edca56c355d028a4a062b409e78fc0
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2021-03-18 16:16:59 +0000

    Update Products.js

--- frontend/src/components/Pages/Products.js
+++ frontend/src/components/Pages/Products.js
@@ -1,6 +1,66 @@
-import React from "react";
-import "../../App.css"
+import React, {useState} from "react";
+import { NavLink } from "react-router-dom";
+import axios from "axios"
+import "./Products.scss"
+// import "../../App.css"
 
-export default function Products() {
-    return <h1 className="products">Products</h1>
+const Products = ({history}) => {
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    const [markdown, setMarkdown] = useState("")
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        const data = {title, description, markdown}
+
+        const articleData = async () => {
+            const blogPost = await axios.post("/api/articles", data)
+            return blogPost
+        }
+        articleData()
+        history.push("/")
+    }
+    return (
+        <div className="form">
+            <h1>New Article</h1>
+            <form onSubmit={submitHandler}>
+                <div className="form__fields">
+                    <input 
+                    type="text"
+                    name="title"
+                    id="title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    className="form-control"
+                    placeholder="enter the title"
+                    required
+                    />
+                    <textarea 
+                    name="description"
+                    id="description"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    className="form-control"
+                    placeholder="write the description"
+                    required
+                    ></textarea>
+                    <textarea 
+                    name="markdown"
+                    id="markdown"
+                    value={markdown}
+                    onChange={(e) => setMarkdown(e.target.value)}
+                    className="form-control"
+                    placeholder="write the markdown"
+                    required
+                    ></textarea>
+                </div>
+                <NavLink to="/">
+                    cancle
+                </NavLink>
+                <button type="submit" className="form__btn">Save</button>
+            </form>
+        </div>
+    )
 }
+
+export default Products
