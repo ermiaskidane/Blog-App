1ae4a76aa46241c732b46ac6f44ac502e522e702
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2021-04-23 16:20:34 +0100

    Update Paginate.js

--- frontend/src/components/Paginate/Paginate.js
+++ frontend/src/components/Paginate/Paginate.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { Link } from "react-router-dom"
 import "./Paginate.scss"
 
-const Paginate = ({ pages, page, keyword = '' }) => {
+const Paginate = ({path, pages, page, keyword = '' }) => {
   return (
     pages > 1 && (
       <div className="pagination">
@@ -13,7 +13,7 @@ const Paginate = ({ pages, page, keyword = '' }) => {
             to={
                keyword
                   ? `/search/${keyword}/page/${x + 1}`
-                  : `/blogs/all/${x + 1}`
+                  : `/blogs/${path}/${x + 1}`
             }
           >
             <p active={(x + 1 === page).toString()}>{x + 1}</p>
