4b77e56b8c117fb980b835ba24d4e4e90ea40f56
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2021-04-24 14:58:05 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react"
 import './App.scss';
 import Navbar from "./components/Navbar/Navbar"
 import Home from "./components/Pages/Home"
-import Services from "./components/Pages/Services"
 import AddBlog from "./components/Pages/AddBlog/AddBlog"
 import UpdateArticle from "./components/Pages/Update/UpdateArticle"
 import EditArticle from "./components/Pages/Update/EditArticle"
@@ -24,7 +23,6 @@ const App = () => {
             <Route path="/blogs/all/:pageNumber" component={AllBlogs} exact />
             <Route path="/blog/:slug"  component={Blog}/>
             <Route path="/" exact  component={Home}/>
-            <Route path="/services"  component={Services}/>
             <Route path="/products"  component={AddBlog}/>
             <Route path="/edit"  exact component={EditArticle}/>
             <Route path="/blogs/edit/:pageNumber" component={EditArticle} exact />
