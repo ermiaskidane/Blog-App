f3821ae62aa34451fb0c0c64b777cb0449f5a158
ermiaskidane <35461600+ermiaskidane@users.noreply.github.com>
2021-04-27 16:46:59 +0100

    Update Loader.js

--- frontend/src/components/Loader/Loader.js
+++ frontend/src/components/Loader/Loader.js
@@ -1,18 +1,25 @@
-import React from "react"
-// import { Spinner } from "react-bootstrap"
+import React from 'react';
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
+}));
 
 const Loader = () => {
+    const classes = useStyles();
+  
     return (
-        <div 
-            style={{ 
-                width: "100px",
-                height: "100px", 
-                margin: "auto", 
-                display: "block"
-            }}>
-            <span className="sr-only">Loading...</span>
-        </div>
-    )
-}
+      <div>
+        <Backdrop className={classes.backdrop} open>
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      </div>
+    );
+  }
 
-export default Loader
+  export default Loader
