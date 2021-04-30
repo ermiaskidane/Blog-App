29c9c3fa53e2122ec3b4f755a47004448db4a740
ermias kidane <hadeklte21@gmail.com>
2021-04-30 21:16:04 +0100

    make it better

--- backend/server.js
+++ backend/server.js
@@ -16,16 +16,26 @@ const app = express()
 
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("Api is running")
-})
+// app.get("/", (req, res) => {
+//     res.send("Api is running")
+// }) 
 
 app.use("/api/users", userRoutes)
 app.use("/api/articles", articleRoutes)
 app.use("/api/upload", uploadRoutes)
-
+ 
 const __dirname = path.resolve() // c/z we use ES6 module we need to mimic
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/build")))
+
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")))
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running...")
+    })
+}
 
 // // Custom Error Handler for Routes does not exist
 // app.use(notFound);
